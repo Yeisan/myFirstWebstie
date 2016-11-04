30f82ef5a98a526e61f7e6b7bc3687d431de043b
Krieg Benally <kriegbenally@gmail.com>
2016-11-03 19:36:39 -0600

    added stuff

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE Html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dan's Site</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="lib/css/style.css">
+
+  </head>
+  <body>
+
+    <?php require_once("lib/partials/navbar.php");?>
+
+    <h1> This is Dans second <em>Website</em></h1>
+      <div class="container">
+        <h1> About Me? </h1>
+      </div>
+      <p> I will update the listing.</p>
+      <ul>
+        <li>I hate cats </li>
+        <li>I dislike Animals that are not anthromorphic</li>
+        <li>If you like pokemon, we can get along...</li>
+        <li>まんこがですじ<li>
+      </ul>
+
+      <div class="boxModel">
+        <h2>content</h2>
+      </div>
+  </body>
+</html>
